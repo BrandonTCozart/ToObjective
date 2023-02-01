51927c54a958a0946aefad35f3277f4b1484f83e
BrandonTCozart <brandon.cozart2000@gmail.com>
2023-02-01 16:06:01 -0500

    Close to end of day

--- ToObjective/Controllers/ObjectiveController.cs
+++ ToObjective/Controllers/ObjectiveController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToObjective.Data;
+using ToObjective.Models;
 
 namespace ToObjective.Controllers
 {
+    //[ApiController]
+    //[Route("api/[controller]")]
     public class ObjectiveController : Controller
     {
+        private readonly ObjectiveDbContext _db;
+
+        public ObjectiveController(ObjectiveDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Objective> objectivesList = _db.Objectives;
+            return View(objectivesList);
         }
+
+        /*
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            return Ok(await _db.Objectives.ToListAsync());
+        } 
+        */
+
+
+
     }
 }
